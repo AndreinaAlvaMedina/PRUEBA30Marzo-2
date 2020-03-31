da661e5247ac5b89a8833b2890d685e4e3fcb42c
DenilsonRamirez <denixmas123@gmail.com>
2020-03-30 18:37:19 -0600

    Update Prueba30marzo.java

--- Prueba30Marzo/src/Prueba30marzo.java
+++ Prueba30Marzo/src/Prueba30marzo.java
@@ -17,6 +17,11 @@ public class Prueba30marzo {
         System.out.println("Carlos Andrés Miranda Méndez");
         System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
         System.out.println("58385781");
+        
+        
+        System.out.println("Nombre: Denilson Baltazar Ramírez Andrés");
+        System.out.println("Direccion: Barillas Zona 6");
+        System.out.println("Telefono: 46400006");
     }
     
 }
